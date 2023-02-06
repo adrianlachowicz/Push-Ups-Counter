8ca3a43db1b5084c0def8f59d3bd62671396eb10
Adrian Lachowicz <adrianlachowicz36@gmail.com>
2023-02-06 15:15:40 +0100

    Added the extract_landmarks_from_image function, which extracts landmarks from specific image and returns single DataFrame row

--- utils/extract_landmarks.py
+++ utils/extract_landmarks.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import pandas as pd
 import mediapipe as mp
 import matplotlib.pyplot as plt
 from argparse import ArgumentParser
@@ -12,7 +13,9 @@ def parse_args():
     Returns:
         arguments (Namespace) - Parsed arguments.
     """
-    parser = ArgumentParser(description="The script extracts body landmarks from passed image.")
+    parser = ArgumentParser(
+        description="The script extracts body landmarks from passed image."
+    )
     parser.add_argument("--image-path", help="A path to a image.")
     args = parser.parse_args()
     return args
@@ -20,7 +23,7 @@ def parse_args():
 
 def load_image(image_path: str):
     """
-    The function loads image from a passed path using the OpenCV library. 
+    The function loads image from a passed path using the OpenCV library.
 
     Arguments:
         image_path (str) - A specific image path.
@@ -32,7 +35,7 @@ def load_image(image_path: str):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     return img
-    
+
 
 def load_mediapipe_model():
     """
@@ -44,13 +47,85 @@ def load_mediapipe_model():
 
     mp_pose = mp.solutions.pose
 
-    pose = mp_pose.Pose(static_image_mode=True,
-                    model_complexity=2,
-                    enable_segmentation=True,
-                    min_detection_confidence=0.5)
+    pose = mp_pose.Pose(
+        static_image_mode=True,
+        model_complexity=2,
+        enable_segmentation=True,
+        min_detection_confidence=0.5,
+    )
 
     return pose
 
+
+def extract_landmarks_from_image(img: np.array, model: mp.solutions.pose.Pose):
+    """
+    The function extracts landmarks from image using the Mediapipe model and returns appropriate coordinates.
+
+    Arguments:
+        img (np.array) - A image
+        model (mp.solutions.pose.Pose) - The Mediapipe model, which predicts pose landmarks
+
+    Returns:
+        coordinates_df (pd.DataFrame) - A DataFrame, which contains coordinates in separate columns.
+    """
+
+    results = model.process(img)
+
+    # Variable naming convention
+    #   first fragment:    l -> left   /   r -> right
+    #   second fragment:   sh -> shoulder   /   el -> elbow   /   wr -> wrist
+    #   third fragment:    x -> The X axis coordinate   /   y -> The Y axis coordinate
+
+    # Left shoulder landmarks
+    l_sh = results.pose_landmarks.landmark[mp.solutions.pose.PoseLandmark.LEFT_SHOULDER]
+    l_sh_x = l_sh.x
+    l_sh_y = l_sh.y
+
+    # Left elbow landmarks
+    l_el = results.pose_landmarks.landmark[mp.solutions.pose.PoseLandmark.LEFT_ELBOW]
+    l_el_x = l_el.x
+    l_el_y = l_el.y
+
+    # Left wrist landmarks
+    l_wr = results.pose_landmarks.landmark[mp.solutions.pose.PoseLandmark.LEFT_WRIST]
+    l_wr_x = l_wr.x
+    l_wr_y = l_wr.y
+
+    # Right shoulder landmarks
+    r_sh = results.pose_landmarks.landmark[
+        mp.solutions.pose.PoseLandmark.RIGHT_SHOULDER
+    ]
+    r_sh_x = r_sh.x
+    r_sh_y = r_sh.y
+
+    # Right elbow landmarks
+    r_el = results.pose_landmarks.landmark[mp.solutions.pose.PoseLandmark.RIGHT_ELBOW]
+    r_el_x = r_el.x
+    r_el_y = r_el.y
+
+    # Right wrist landmarks
+    r_wr = results.pose_landmarks.landmark[mp.solutions.pose.PoseLandmark.RIGHT_WRIST]
+    r_wr_x = r_wr.x
+    r_wr_y = r_wr.y
+
+    data = {
+        "LEFT_SHOULDER_X": l_sh_x,
+        "LEFT_SHOULDER_Y": l_sh_y,
+        "LEFT_ELBOW_X": l_el_x,
+        "LEFT_ELBOW_Y": l_el_y,
+        "LEFT_WRIST_X": l_wr_x,
+        "LEFT_WRIST_Y": l_wr_y,
+        "RIGHT_SHOULDER_X": l_sh_x,
+        "RIGHT_SHOULDER_Y": r_sh_y,
+        "RIGHT_ELBOW_X": r_el_x,
+        "RIGHT_ELBOW_Y": r_el_y,
+        "RIGHT_WRIST_X": r_wr_x,
+        "RIGHT_WRIST_Y": r_wr_y,
+    }
+
+    coordinates_df = pd.DataFrame(data, index=[0])
+
+    return coordinates_df
 
 
 if __name__ == "__main__":
@@ -61,6 +136,4 @@ if __name__ == "__main__":
     model = load_mediapipe_model()
     img = load_image(image_path)
 
-
-    plt.imshow(img)
-    plt.show()
+    extract_landmarks_from_image(img, model)
