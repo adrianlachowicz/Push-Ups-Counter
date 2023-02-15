e27e5ab243117c9e0dc32126dccb4bc1feefac4c
Adrian Lachowicz <adrianlachowicz36@gmail.com>
2023-02-15 14:11:33 +0100

    Added a train script with whole pipeline

--- src/train.py
+++ src/train.py
@@ -7,7 +7,11 @@ from config import *
 from model import Model
 from dataset import PushUpsDataset
 from torch.utils.data import DataLoader
-from sklearn.metrics import accuracy_score, balanced_accuracy_score, precision_recall_fscore_support
+from sklearn.metrics import (
+    accuracy_score,
+    balanced_accuracy_score,
+    precision_recall_fscore_support,
+)
 
 
 def init_wandb():
@@ -50,9 +54,7 @@ def load_dataloaders():
     val_dataloader = DataLoader(
         val_ds, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, shuffle=True
     )
-    test_dataloader = DataLoader(
-        test_ds, batch_size=BATCH_SIZE, shuffle=True
-    )
+    test_dataloader = DataLoader(test_ds, batch_size=BATCH_SIZE, shuffle=True)
 
     return train_dataloader, val_dataloader, test_dataloader
 
@@ -73,9 +75,10 @@ def validate_model(model, val_dataloader):
     """
     The function validates model after each epoch and logs calculated metrics to the WandB service.
 
-    
+    Arguments:
+        model (nn.Module) - The model.
+        val_dataloader (DataLoader) - A validation dataloader.
     """
-    print("Validating model starting .............................")
 
     running_loss = 0.0
     outputs_ = []
@@ -86,12 +89,13 @@ def validate_model(model, val_dataloader):
     for data in val_dataloader:
         inputs, targets = data[0].to(DEVICE), data[1].to(DEVICE)
         outputs = model(inputs)
-
         loss = model.criterion(outputs, targets)
 
         running_loss += loss.item()
 
-        outputs_.extend(torch.softmax(outputs, dim=1).cpu().detach().numpy().argmax(1).tolist())
+        outputs_.extend(
+            torch.softmax(outputs, dim=1).cpu().detach().numpy().argmax(1).tolist()
+        )
         targets_.extend(targets.cpu().detach().numpy().tolist())
 
         pbar.update(1)
@@ -101,34 +105,104 @@ def validate_model(model, val_dataloader):
     accuracy = accuracy_score(targets_, outputs_)
     balanced_accuracy = balanced_accuracy_score(targets_, outputs_)
 
-    precision, recall, f_beta, support = precision_recall_fscore_support(targets_, outputs_)
+    precision, recall, f_beta, support = precision_recall_fscore_support(
+        targets_, outputs_
+    )
 
     mean_precision = sum(precision) / len(precision)
     mean_recall = sum(recall) / len(recall)
     mean_f_beta = sum(f_beta) / len(f_beta)
     mean_support = sum(support) / len(support)
 
-    wandb.log({
-        "valid/loss": mean_loss,
-        "valid/accuracy": accuracy,
-        "valid/balanced_accuracy": balanced_accuracy,
-        "valid/precision": mean_precision,
-        "valid/recall": mean_recall,
-        "valid/f_beta": mean_f_beta,
-        "valid/support": mean_support
-    })
+    wandb.log(
+        {
+            "valid/loss": mean_loss,
+            "valid/accuracy": accuracy,
+            "valid/balanced_accuracy": balanced_accuracy,
+            "valid/precision": mean_precision,
+            "valid/recall": mean_recall,
+            "valid/f_beta": mean_f_beta,
+            "valid/support": mean_support,
+        }
+    )
 
-    print("Validating model ended ................................")
 
-
-def train_model():
+def train_model(model, train_dataloader, val_dataloader, checkpoints_path):
     """
     The function trains model
-    :return:
+
+    Arguments:
+        model (nn.Module) - The model.
+        train_dataloader (DataLoader) - A training dataloader.
+        val_dataloader (DataLoader) - A validation dataloader.
+        checkpoints_path (str) - A path to directory, where script will save all model checkpoints.
     """
 
-_, _, loader = load_dataloaders()
-model = load_model().to(DEVICE)
+    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
-init_wandb()
-validate_model(model, loader)
+    for epoch in range(EPOCHS):
+        step_p_bar = tqdm(total=len(train_dataloader))
+
+        running_loss = 0.0
+        outputs_ = []
+        targets_ = []
+
+        for data in train_dataloader:
+            inputs, targets = data[0].to(DEVICE), data[1].to(DEVICE)
+            outputs = model(inputs)
+            loss = model.criterion(outputs, targets)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            outputs_.extend(
+                torch.softmax(outputs, dim=1).cpu().detach().numpy().argmax(1).tolist()
+            )
+            targets_.extend(targets.cpu().detach().numpy().tolist())
+
+            step_p_bar.update(1)
+
+        # Calculate metrics
+        mean_loss = running_loss / len(train_dataloader)
+        accuracy = accuracy_score(targets_, outputs_)
+        balanced_accuracy = balanced_accuracy_score(targets_, outputs_)
+
+        precision, recall, f_beta, support = precision_recall_fscore_support(
+            targets_, outputs_
+        )
+
+        mean_precision = sum(precision) / len(precision)
+        mean_recall = sum(recall) / len(recall)
+        mean_f_beta = sum(f_beta) / len(f_beta)
+        mean_support = sum(support) / len(support)
+
+        wandb.log(
+            {
+                "train/loss": mean_loss,
+                "train/accuracy": accuracy,
+                "train/balanced_accuracy": balanced_accuracy,
+                "train/precision": mean_precision,
+                "train/recall": mean_recall,
+                "train/f_beta": mean_f_beta,
+                "train/support": mean_support,
+            }
+        )
+        validate_model(model, val_dataloader)
+
+        checkpoint_model_path = os.path.join(
+            checkpoints_path, "epoch-{}-model.pth".format(epoch + 1)
+        )
+        torch.save(model.state_dict(), checkpoint_model_path)
+
+
+if __name__ == "__main__":
+    CHECKPOINTS_PATH = os.path.join("src", "models", TRAIN_NAME)
+    os.makedirs(CHECKPOINTS_PATH, exist_ok=True)
+
+    train_loader, val_loader, test_loader = load_dataloaders()
+    model = load_model().to(DEVICE)
+
+    init_wandb()
+    train_model(model, train_loader, val_loader, CHECKPOINTS_PATH)
