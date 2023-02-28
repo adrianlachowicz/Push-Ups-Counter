bdb656a6bb893df8769b41553766ceffc49b90e4
Adrian Lachowicz <adrianlachowicz36@gmail.com>
2023-02-28 22:08:50 +0100

    Added a script, which counts a number of push-ups on a specific video

--- src/app/video_counter.py
+++ src/app/video_counter.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+from predict import predict
 from argparse import ArgumentParser
 
 
@@ -27,19 +28,57 @@ def run_counter(video_path: str):
     Arguments:
         video_path (str) - A path to an video
     """
-    counter = 1
+    valid_labels = [0, 1]
+    last_valid_label = -1
+    previous_label = -1
+    push_ups_counter = 0
+
+    
+    stage_up = False
+    stage_down = False
+    last_stage_up = False
 
     cap = cv2.VideoCapture(video_path)
-    count = 0
-    counter += 1
-    success = True
+    frame_counter = 0
 
-    while success:
-        success,image = cap.read()
-        print('read a new frame:',success)
-        if count%30 == 0 :
-            cv2.imwrite("/workspaces/Push-Ups-Counter/frames/{}.jpg".format(counter), image)
-        count+=1
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+
+        if ret == True:
+            cv2.imshow('Frame', frame)
+          
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+
+            # Make predictions every 1 second
+            if frame_counter % 30 == 0:
+                label = predict(frame)
+
+                if label in valid_labels:
+                    last_valid_label = label
+                
+                if last_valid_label == 0:
+                    stage_down = True
+                elif last_valid_label == 1:
+                    stage_up = True
+
+                if previous_label == 0 and last_valid_label == 1:
+                    last_stage_up = True
+
+                if stage_up and stage_down and last_stage_up:
+                    push_ups_counter += 1
+                    stage_up = False
+                    stage_down = False
+                    last_stage_up = False
+
+                previous_label = last_valid_label
+                print(push_ups_counter)
+
+            frame_counter += 1
+            
+        else:
+            break
+    
 
 
 if __name__ == "__main__":
